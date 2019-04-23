c06a4e8c4a476bcbaad7f413715bf7494937dafd
tangtaocode <13632678082@163.com>
2019-04-23 21:45:40 +0800

    登录拦截器

--- eoms-parent/eoms-commons/src/main/java/eoms/cn/commons/filter/LoginTokenFilter.java
+++ eoms-parent/eoms-commons/src/main/java/eoms/cn/commons/filter/LoginTokenFilter.java
@@ -1,6 +1,7 @@
  package eoms.cn.commons.filter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,6 +9,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import eoms.cn.commons.response.EomsResponse;
+import eoms.cn.commons.tools.FastJsonUtils;
 /**
  * 
  * @author TTCODE
@@ -33,5 +38,20 @@ public class LoginTokenFilter implements Filter {
         // TODO Auto-generated method stub
          
     }
-
+    
+    private boolean showTip(HttpServletResponse response, EomsResponse resp) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/json; charset=utf-8");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.append(FastJsonUtils.toJSONString(response));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            out.close();
+        }
+        return false;
+    }
 }
